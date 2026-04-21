aa163e65c730c0465b61fe147fc44811f74cd2c1
Krishna Sangal <Krishnasangal3@gmail.com>
2026-04-21 22:27:45 +0530

    Project Created 09-04-2026

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './Pages/HomePage'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+export default function App() {
+  return (
+    <>
+        <BrowserRouter>
+        <Navbar/>
+        <Routes>
+           <Route path='' element={<HomePage />}   />
+        </Routes>
+        <Footer/>
+        </BrowserRouter>
+    </>
+  )
+}
+
+
+
+
+
+
